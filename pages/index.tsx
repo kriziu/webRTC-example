e9bad8377125b12433834b3c1c97cd922a29d4a6
kriziu <brunodzi07@gmail.com>
2022-03-31 20:10:40 +0200

    created simple example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,121 @@
+/* eslint-disable no-param-reassign */
+import { RefObject, useEffect, useRef, useState } from 'react';
+
 import type { NextPage } from 'next';
+import type Peer from 'peerjs';
+import { useList } from 'react-use';
+import { io } from 'socket.io-client';
+
+const addVideoStream = (
+  scVideoRef: RefObject<HTMLVideoElement>,
+  stream: MediaStream
+) => {
+  const video = scVideoRef.current;
+  if (!video) return;
+
+  video.srcObject = stream;
+  video.style.display = 'block';
+  video.addEventListener('loadedmetadata', () => {
+    video.play();
+  });
+};
+
+const closeVideoStream = (scVideoRef: RefObject<HTMLVideoElement>) => {
+  const remoteVideo = scVideoRef.current;
+  if (!remoteVideo) return;
+
+  remoteVideo.srcObject = null;
+  remoteVideo.style.display = 'none';
+};
 
 const Home: NextPage = () => {
+  const [socket] = useState(io);
+  const [peer, setPeer] = useState<Peer>();
+  const [myStream, setMyStream] = useState<MediaStream>();
+
+  const [calls, callsHandler] = useList<Peer.MediaConnection>();
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const scVideoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((stream) => {
+        import('peerjs').then(({ default: Peer }) => {
+          const newPeer = new Peer(socket.id, {
+            host: '/',
+            port: 3000,
+
+            path: 'peerjs',
+          });
+
+          if (peer?.id !== newPeer.id) {
+            setPeer(newPeer);
+          }
+
+          setMyStream(stream);
+        });
+      });
+
+    peer?.on('open', () => socket.emit('join-room', 'room'));
+  }, [peer, socket]);
+
+  useEffect(() => {
+    if (!myStream || !peer) return;
+
+    addVideoStream(videoRef, myStream);
+
+    socket.on('user-connected', (userId) => {
+      const call = peer.call(userId, myStream);
+      if (!call) return;
+
+      callsHandler.push(call);
+
+      call.on('stream', (remoteStream) => {
+        addVideoStream(scVideoRef, remoteStream);
+      });
+
+      call.on('close', () => {
+        closeVideoStream(scVideoRef);
+      });
+    });
+
+    socket.on('user-disconnected', (userId) => {
+      calls.find((call) => call.peer === userId)?.close();
+      callsHandler.filter((call) => call.peer !== userId);
+    });
+
+    // eslint-disable-next-line consistent-return
+    return () => {
+      socket.off('user-connected');
+      socket.off('user-disconnected');
+    };
+  }, [calls, callsHandler, myStream, peer, socket]);
+
+  useEffect(() => {
+    const handlePeerCall = (call: Peer.MediaConnection) => {
+      call.answer(myStream);
+      callsHandler.push(call);
+
+      call.on('stream', (remoteStream) => {
+        addVideoStream(scVideoRef, remoteStream);
+      });
+
+      call.on('close', () => closeVideoStream(scVideoRef));
+    };
+
+    peer?.on('call', handlePeerCall);
+
+    return () => {
+      peer?.off('call', handlePeerCall);
+    };
+  }, [callsHandler, myStream, peer]);
+
   return (
     <div className="flex h-full w-full items-center justify-center">
-      Template
+      <video muted ref={videoRef} />
+      <video ref={scVideoRef} />
     </div>
   );
 };
--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,8 @@ import { createServer } from 'http';
 
 import express from 'express';
 import next, { NextApiHandler } from 'next';
-// import { Server } from 'socket.io';
+import { ExpressPeerServer } from 'peer';
+import { Server } from 'socket.io';
 
 const port = parseInt(process.env.PORT || '3000', 10);
 const dev = process.env.NODE_ENV !== 'production';
@@ -14,24 +15,35 @@ nextApp.prepare().then(async () => {
   const app = express();
   const server = createServer(app);
 
-  // const io = new Server(server);
+  const io = new Server(server);
+
+  const peerServer = ExpressPeerServer(server);
+
+  // peerServer.on('connection', () => console.log('connected to webRTC'));
+
+  app.use('/peerjs', peerServer);
 
   // app.get('/hello', async (_, res) => {
   //   res.send('Hello World');
   // });
 
-  // io.on('connection', socket => {
-  //   console.log('connection');
-  //   socket.emit('status', 'Hello from Socket.io');
+  io.on('connection', (socket) => {
+    console.log('connected to socket.io');
+    socket.emit('status', 'Hello from Socket.io');
 
-  //   socket.on('send', data => {
-  //     socket.broadcast.emit('new_message', data);
-  //   });
+    socket.on('join-room', (roomId: string) => {
+      socket.join(roomId);
+      console.log(socket.id);
 
-  //   socket.on('disconnect', () => {
-  //     console.log('client disconnected');
-  //   });
-  // });
+      socket.broadcast.to(roomId).emit('user-connected', socket.id);
+    });
+
+    socket.on('disconnecting', () => {
+      const roomId = [...socket.rooms][1];
+      console.log([...socket.rooms][1]);
+      socket.broadcast.to(roomId).emit('user-disconnected', socket.id);
+    });
+  });
 
   app.all('*', (req: any, res: any) => nextHandler(req, res));
 
